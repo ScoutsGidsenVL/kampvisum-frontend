b0fb55e84cfb97c37570650a0bdd966b86514f70
ricardo <ricardo@inuits.eu>
2021-08-02 17:43:52 +0200

    Ref #80465, update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld />
+    <div>
+      <MyName name="ricardo" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from 'vue'
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { HelloWorld, MyName } from 'vue-3-component-library'
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
+    MyName,
   },
-});
+})
 </script>
