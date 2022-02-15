e2089f72de42678402ce7f90414a8fe23879d5d9
Jeroen Wouters <jeroenwouters@inuits.eu>
2022-02-15 21:49:27 +0100

    fix issue in prod for sectionTitles

--- src/helpers/sectionsHelper.ts
+++ src/helpers/sectionsHelper.ts
@@ -1,25 +1,24 @@
-import { Camp } from "@/serializer/Camp"
+import { Camp } from '@/serializer/Camp'
 import { ref } from 'vue'
 
 export const useSectionsHelper = (): {
-  getSectionsTitle: (camp: Camp) => string,
+  getSectionsTitle: (camp: Camp) => string
   genderLabel: (gender: string) => string
   ageGroupLabel: (gender: string) => string
 } => {
-
-
   const getSectionsTitle = (camp: Camp): string => {
     const sectionsTitle = ref<string>('')
-
+    const sections: string[] = []
     if (camp && camp.sections) {
-      camp.sections.forEach((section: any, index: any) => {
-        if (camp.sections && (camp.sections.length - 1) > (index)) {
-          sectionsTitle.value += section.name.name + ' & '
-        } else {
-          sectionsTitle.value += section.name.name
+      camp.sections.forEach((section: any) => {
+        if (section.name.name) {
+          sections.push(section.name.name)
         }
       })
     }
+
+    sectionsTitle.value = sections.join(' & ')
+
     return sectionsTitle.value
   }
 
@@ -108,6 +107,6 @@ export const useSectionsHelper = (): {
   return {
     getSectionsTitle,
     genderLabel,
-    ageGroupLabel
+    ageGroupLabel,
   }
 }
