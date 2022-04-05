19b10786833b96da701e3ce11937b903f1fa2439
ricardo <ricardo@inuits.eu>
2022-04-05 09:58:45 +0200

    Ref: #91770, adjusted z-index to avoid closing the sidebar in the wrong way because it resets the scroll state.

--- src/components/sideBars/DeadlinesSidebar.vue
+++ src/components/sideBars/DeadlinesSidebar.vue
@@ -228,6 +228,8 @@ export default defineComponent({
       if (!isUpdatingFlag.value) {
         isUpdatingFlag.value = true
         if (flag.id && deadline.id) {
+          console.log('RICARDO TOGGLE FLAG 1: ', flag)
+          console.log('RICARDO TOGGLE FLAG 2: ', deadline)
           await RepositoryFactory.get(DeadlineRepository)
             .updateFlag(deadline.id, flag.id, { flag: flag.flag })
             .then((deadline: Deadline) => {
