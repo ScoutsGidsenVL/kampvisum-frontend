84f161567a8d68e73cec80e4200e9d2dd7f24241
ricardo <ricardo@inuits.eu>
2022-04-08 13:30:35 +0200

    Fixes

--- src/helpers/campHelper.ts
+++ src/helpers/campHelper.ts
@@ -35,7 +35,7 @@ export const useCampHelper = (): {
         })
 
         if (selectedGroup.value.groupAdminId !== c.groupGroupAdminId) { 
-          goToHome()
+          // goToHome()
         }
 
         return c
