476c7525a4e1a55f178f051c8cfa21c47bfb3bcf
ricardo <ricardo@inuits.eu>
2022-06-30 15:49:10 +0200

    Ref: small fix

--- src/components/cards/leaflet/CampsOverviewMap.vue
+++ src/components/cards/leaflet/CampsOverviewMap.vue
@@ -1,31 +1,6 @@
 <template>
   <div class="w-full" style="height: 60vh">
-    <l-map :use-global-leaflet="true" ref="map" :options="options" class="z-0" :maxZoom="10"   v-model:zoom="zoom" :center="center" @ready="onLeafletReady">
-      <!-- <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
-      <marker-cluster
-        :options="{ showCoverageOnHover: false, chunkedLoading: true }"
-      >
-        <l-marker :lat-lng="[47.7515953048815, 8.757179159967961]" />
-        <l-marker :lat-lng="[51.42494690949777, 6.901031944520698]" />
-        <l-marker :lat-lng="[51.42494690949777, 6.901031944520698]" />
-      </marker-cluster> -->
-
-      <!-- <div v-for="location in locations" :key="location">
-          <l-marker :lat-lng="[location.latitude, location.longitude]">
-            <l-icon>
-              <svg width="30" height="30" style="margin-left: -14px; margin-top: -30px" viewBox="0 0 19 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M18.875 9.41675C18.875 16.7084 9.5 22.9584 9.5 22.9584C9.5 22.9584 0.125 16.7084 0.125 9.41675C0.125 6.93034 1.11272 4.54578 2.87087 2.78762C4.62903 1.02947 7.0136 0.041748 9.5 0.041748C11.9864 0.041748 14.371 1.02947 16.1291 2.78762C17.8873 4.54578 18.875 6.93034 18.875 9.41675Z"
-                  fill="#7B8F1C"
-                />
-              </svg>
-            </l-icon>
-            <l-popup class="w-60">
-              <camp-custom-popup :location="location" />
-            </l-popup>
-          </l-marker>
-      </div> -->
-
+    <l-map :use-global-leaflet="true" ref="map" :options="options" class="z-0" :maxZoom="19"   v-model:zoom="zoom" :center="center" @ready="onLeafletReady">
       <template v-if="leafletReady">
       <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
       <marker-cluster
@@ -104,7 +79,8 @@ export default defineComponent({
   methods: {
     async onLeafletReady() {
       await this.$nextTick();
-      this.leafletObject = this.$refs.map.leafletObject;
+    const map = ref<any>(null)
+      this.leafletObject = map.leafletObject;
       this.leafletReady = true;
     },
   },
