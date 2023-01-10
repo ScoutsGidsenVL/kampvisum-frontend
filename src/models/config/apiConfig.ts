687ff728e520f13b159ff1fab94178233b9761b5
Ricardo Acosta Torres <ricardo@inuits.eu>
2023-01-10 15:53:48 +0100

    Revert "Revert "Ref: #91640, implemented logout button""
    This reverts commit 85ee670d470dc01348a710004c598333a125d45a.

--- src/models/config/apiConfig.ts
+++ src/models/config/apiConfig.ts
@@ -3,9 +3,11 @@ import Serializable from '../../interfaces/serializable'
 export default class ApiConfig implements Serializable<ApiConfig> {
   baseUrl!: string
   apiSuffix!: string
+  logoutUrl!: string
 
   deserialize(input: any) {
     this.baseUrl = input.baseUrl
+    this.logoutUrl = input.logoutUrl
     this.apiSuffix = input.apiSuffix
 
     if (!this.baseUrl) {
