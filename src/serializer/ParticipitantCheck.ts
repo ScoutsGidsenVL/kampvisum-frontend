a4bde502883e1ec8b99ecad7637bce1ec6709fdc
Stijn Verholen <boro@inuits.eu>
2023-01-19 10:31:19 +0100

    fix: allow empty participant list

--- src/serializer/ParticipitantCheck.ts
+++ src/serializer/ParticipitantCheck.ts
@@ -11,9 +11,9 @@ interface participantToPatch {
   group_admin_id?: string
 }
 
-export const ParticipantCheckDeserializer = (input: any): { participantCheckType: string, participants: Member[], count: number} => {
-  const participants: Member[] = input.participants.map((i: any) => MemberDeserializer(i.participant, i.id, i.payment_status))
-  return { participantCheckType: input.participant_check_type , participants:participants, count: input.data_count}
+export const ParticipantCheckDeserializer = (input: any): { participantCheckType: string, participants: Member[], count: number } => {
+  const participants: Member[] = input.participants ? input.participants.map((i: any) => MemberDeserializer(i.participant, i.id, i.payment_status)) : []
+  return { participantCheckType: input.participant_check_type, participants: participants, count: input.data_count }
 }
 
 export const ParticipantCheckSerializer = (input: Member[]): any => {
@@ -21,11 +21,11 @@ export const ParticipantCheckSerializer = (input: Member[]): any => {
   let membersToPatch: Array<any> = []
   input.forEach(member => {
     if (member.isChecked) {
-        if (member.isMember) {
-          membersToPatch.push({ participant: { group_admin_id: member.id } })
-        } else {
-          membersToPatch.push({ participant: { id: member.id } })
-        }
+      if (member.isMember) {
+        membersToPatch.push({ participant: { group_admin_id: member.id } })
+      } else {
+        membersToPatch.push({ participant: { id: member.id } })
+      }
     }
   })
 
