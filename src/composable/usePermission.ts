c1f079d3e2c83343221be334bfbc42d4bc7105bf
Ricardo Acosta Torres <ricardo@inuits.eu>
2023-01-25 14:54:02 +0100

    Ref: Implemented new permissions

--- src/composable/usePermission.ts
+++ src/composable/usePermission.ts
@@ -1,5 +1,12 @@
 import store from '../store/store'
 
+export enum permissions {
+  'CREATE_VISUM' = 'visums.create_visum',
+  'DELETE_VISUM' = 'visums.delete_visum',
+  'UPDATE_VISUM' = 'visums.update_visum',
+  'LIST_VISUM' = 'visums.list_visum'
+}
+
 export const usePermission = (): {
   can: (permission: string) => boolean
 } => {
--- src/views/KampvisumHome.vue
+++ src/views/KampvisumHome.vue
@@ -31,7 +31,7 @@
     </div>
 
     <div class="xs:w-100 md:w-80">
-      <custom-button v-if="!isForbidden && !isFetchingVisums && isInternetActive && years[0] && years[0] === selectedYear" class="w-100" :extraStyle="'w-100'" @click="openCampSideBar()" :isSubmitting="false" :text="t('pages.kampvisum-overview.create-camp-button')">
+      <custom-button v-if="can(permissions.CREATE_VISUM) && !isForbidden && !isFetchingVisums && isInternetActive && years[0] && years[0] === selectedYear" class="w-100" :extraStyle="'w-100'" @click="openCampSideBar()" :isSubmitting="false" :text="t('pages.kampvisum-overview.create-camp-button')">
         <template v-slot:icon>
           <svg xmlns="http://www.w3.org/2000/svg" style="margin-top: -3px" class="h-5 w-5 inline ml-2" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
@@ -50,15 +50,15 @@
 
     <div v-if="!isFetchingVisums">
       <camp-call-to-action v-if="!isForbidden" :visums="visums" />
-      <div class="grid md:grid-cols-2 xs:grid-cols-1 gap-4">
+      <div v-if="can(permissions.LIST_VISUM)" class="grid md:grid-cols-2 xs:grid-cols-1 gap-4">
         <div  v-for="visum in visums" :key="visum.id">
           <camp-info-card class="mt-5" :visum="visum">
             <template v-if="isInternetActive" v-slot:buttons>
-              <svg @click.stop="editVisum(visum)" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-lightGreen cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+              <svg v-if="can(permissions.UPDATE_VISUM)" @click.stop="editVisum(visum)" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-lightGreen cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
               </svg>
 
-              <svg @click.stop="displayWarning(visum)" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-red cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+              <svg v-if="can(permissions.DELETE_VISUM)" @click.stop="displayWarning(visum)" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-red cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
                 <path
                   fill-rule="evenodd"
                   d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
@@ -91,6 +91,7 @@ import { defineComponent, ref } from 'vue'
 import { Visum } from '@/serializer/Visum'
 import { useI18n } from 'vue-i18n'
 import { useInternetHelper } from '@/helpers/internetHelper'
+import { usePermission, permissions } from '@/composable/usePermission'
 
 export default defineComponent({
   name: 'KampvisumHome',
@@ -118,6 +119,7 @@ export default defineComponent({
     const { selectedGroup, selectedYear, years, setSelectedYear, getYearsForGroup, isInBetweenStartAndEnd } = useGroupAndYears()
     const { visums, isFetchingVisums, getVisums, navigateTowardsVisum } = useVisum()
     const { triggerNotification, isForbidden } = useNotification()
+    const { can } = usePermission()
 
     setBreadcrumbs([])
 
@@ -204,7 +206,9 @@ export default defineComponent({
       years,
       t,
       selectedYear,
-      isInBetweenStartAndEnd
+      isInBetweenStartAndEnd,
+      can,
+      permissions
     }
   },
 })
