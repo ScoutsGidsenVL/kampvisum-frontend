d91910d2c3264fd10d1541f6b94373a3e4e679ef
Stijn Verholen <boro@inuits.eu>
2023-02-06 16:52:42 +0100

    fix: group param

--- src/repositories/MemberRepository.ts
+++ src/repositories/MemberRepository.ts
@@ -12,7 +12,7 @@ export class MemberRepository extends BaseRepository {
   serializer = MemberSerializer
 
   search(groupId: string, query: string, group?: string, filter?: Filter, groupID?: string): Promise<any> {
-    return this.get(groupId, `${this.endpoint}all/?term=${query}${(group ? `/${group}` : '')}${filter && filter.gender ? '&gender=' + filter.gender : ''}${filter && filter.ageMin ? `&min_age=${filter.ageMin}` : ''}${filter && filter.ageMax ? `&max_age=${filter.ageMax}` : ''}${filter && filter.type ? `&type=${filter.type}` : ''}&group=${groupID ? groupID : selectedGroup.value.groupAdminId}`, {}).then((response: any) => {
+    return this.get(groupId, `${this.endpoint}all/?term=${query}&group=${group ? group : groupId}${filter && filter.gender ? '&gender=' + filter.gender : ''}${filter && filter.ageMin ? `&min_age=${filter.ageMin}` : ''}${filter && filter.ageMax ? `&max_age=${filter.ageMax}` : ''}${filter && filter.type ? `&type=${filter.type}` : ''}&group=${groupID ? groupID : selectedGroup.value.groupAdminId}`, {}).then((response: any) => {
       const array: any[] = []
       response.forEach((result: Member) => {
         result = MemberDeserializer(result)
