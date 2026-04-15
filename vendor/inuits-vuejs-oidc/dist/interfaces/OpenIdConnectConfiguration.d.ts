cd7db6702f0112c4ddcf50750cd36f89bbac0cc6
Koen De Voegt <kdv@scoutsengisdenvlaanderen.be>
2026-04-15 16:53:23 +0200

    T7306 - include inuits package

---
+++ vendor/inuits-vuejs-oidc/dist/interfaces/OpenIdConnectConfiguration.d.ts
@@ -0,0 +1,16 @@
+export interface OpenIdConnectConfiguration {
+    baseUrl: string;
+    tokenEndpoint: string;
+    authEndpoint: string;
+    logoutEndpoint: string;
+    clientId: string;
+    clientSecret?: string;
+    scope?: string;
+    authorizedRedirectRoute: string;
+    unauthorizedRedirectRoute?: string;
+    serverBaseUrl?: string;
+    serverTokenEndpoint?: string;
+    serverRefreshEndpoint?: string;
+    InternalRedirectUrl?: string;
+    encodeRedirectUrl?: boolean;
+}
