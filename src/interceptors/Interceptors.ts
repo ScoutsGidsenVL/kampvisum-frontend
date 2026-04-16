22209c265e9e5aa7fe82ac94489ea217f80e1d28
TinusVL <TinusVL@users.noreply.github.com>
2026-04-16 17:28:07 +0200

    Fix voor Interceptors.ts na update Axios

--- src/interceptors/Interceptors.ts
+++ src/interceptors/Interceptors.ts
@@ -3,8 +3,10 @@ import { AxiosRequestConfig } from 'axios'
 export class Interceptors {
   public static buildRequestTokenInterceptorCallback = (keycloak:any) => {
     return function (config: AxiosRequestConfig) {
-      const authorization = 'Authorization'
-      config.headers.common[authorization] = `Bearer ${keycloak.token}`
+      if (!config.headers) {
+        config.headers = {}
+      }
+      config.headers['Authorization'] = `Bearer ${keycloak.token}`
       return config
     }
   }
